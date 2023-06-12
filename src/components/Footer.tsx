449e98ac4284c864e867ee86435e3fe217266ff0
r-alik <r-alik@ukr.net>
2023-06-12 13:22:44 +0300

    Footer component created

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="border-t h-16 flex gap-3 justify-between items-center w-11/12 max-w-5xl mx-auto p-2 text-lg text-Txt-dark sm:text-base">
+      contacts:
+    </footer>
+  );
+};
+
+export default Footer;
