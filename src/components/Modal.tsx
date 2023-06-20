8bc1b6e565ccb3f52081cf488c23151b79944bff
r-alik <r-alik@ukr.net>
2023-06-20 23:02:44 +0300

    Gallery item: is in stock info added. Modal: some styles improved

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,24 +10,27 @@ const Modal = (props: ModalShape) => {
   return (
     <dialog id="modal" className="backdrop:bg-Gray/80 p-1 rounded-md">
       <div className="flex justify-between p-2 text-base">
-        <h2>{props.title}</h2>
+        <h2 className="text-Txt-dar font-semibold px-2 text-lg">
+          {props.title}
+        </h2>
         <button
-          className="w-8 h-8 bg-[url('/close-icon.svg')] bg-Gray/80 bg-contain bg-no-repeat bg-center rounded-full outline-transparent border-transparent 
+          className="w-8 h-8 bg-[url('/close-icon.svg')] 
+          bg-Accent-dark bg-contain bg-no-repeat bg-center rounded-full outline-transparent border-transparent 
           transition-all
           hover:border-transparent 
           hover:rotate-90
-          hover:bg-Gray/60
+          hover:bg-Accent-light
           focus-visible:rotate-90
           focus-visible:border-none
           focus-visible:outline-none
-focus-visible:bg-Gray/60
+        focus-visible:bg-Accent-light
           "
           onClick={props.onModalClose}
           type="button"
         ></button>
       </div>
 
-      <img src={props.src} alt="" />
+      <img className="rounded-xl" src={props.src} alt="" />
     </dialog>
   );
 };
--- src/shared/GalleryItem.tsx
+++ src/shared/GalleryItem.tsx
@@ -17,28 +17,34 @@ function GalleryItem({
   return (
     <div
       className="h-full flex flex-col justify-start items-stretch gap-4 border rounded-lg p-2 snap-start 
-    bg-gradient-to-br from-Peach to-white shadow-[10px_10px_20px_#8c8985,_-10px_-10px_20px_#ffffff] "
+    bg-gradient-to-br from-Peach to-white shadow-[10px_10px_20px_#8c8985,_-10px_-10px_20px_#ffffff] 
+    [&>*:not(:first-child)]:px-2
+    "
     >
       <Picture onModalOpen={onModalOpen} picDetails={picDetail} styles={s} />
-      <div className="flex justify-between gap-1 items-center p-2">
-        <span
-          title={picDetail.isAvailable ? 'В наявності' : 'Немає у наявності'}
-          className={`w-3 h-3 rounded-full ${
-            picDetail.isAvailable ? 'bg-green-500' : 'bg-red-500'
-          }`}
-        ></span>
+      <div className="flex justify-between gap-1 items-center border-b border-dotted">
         <h4 className="mr-auto">{picDetail.title}</h4>
         <span className="text-xs">id: {picDetail.id}</span>
       </div>
 
       <p>{picDetail.description}</p>
-      <p className="mb-3">
+
+      <p>
         Ціна:{' '}
         <span className="text-Accent-dark font-semibold">
           {' '}
           {picDetail.price}
         </span>{' '}
         <span>гривень</span>
+      </p>
+
+      <p className="mb-3">
+        Наявність:
+        {picDetail.isAvailable ? (
+          <span className="ml-2 text-green-600 italic">В наявності</span>
+        ) : (
+          <span className="ml-2 text-Accent-dark italic">Під замовлення</span>
+        )}
       </p>
     </div>
   );
