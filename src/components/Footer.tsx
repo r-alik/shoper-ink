37b0e70ada0b583e8594fb880a86f7376b304e48
r-alik <r-alik@ukr.net>
2023-07-15 19:25:22 +0300

    Years of website development added to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
   const onlyWidth = useWindowWidth();
 
   return (
-    <footer className="min-h-[25vh] text-lg text-white bg-Gray-dark py-6">
+    <footer className="flex flex-col justify-between min-h-[25vh] text-lg text-white bg-Gray-dark pt-6">
       <div className="flex flex-col sm:flex-row gap-10 sm:gap-3 justify-around items-center w-11/12 max-w-5xl mx-auto">
         <div>
           <img
@@ -32,13 +32,13 @@ const Footer = () => {
         </a>
 
         <ul className="flex gap-6 justify-center items-center">
-          <li className="hover:scale-105 transition-all" title="Написати листа">
+          <li className="hover:scale-110 transition-all" title="Написати листа">
             <a target="_blank" href="email:qwerty@qwerty.com">
               <img src={email} width="28" alt="" />
             </a>
           </li>
           <li
-            className="hover:scale-105 transition-all"
+            className="hover:scale-110 transition-all"
             title="Перейти на сторінку в Інстаграм"
           >
             <a target="_blank" href="https://www.instagram.com">
@@ -47,7 +47,7 @@ const Footer = () => {
           </li>
 
           <li
-            className="hover:scale-105 transition-all"
+            className="hover:scale-110 transition-all"
             title="Перейти на сторінку Фейсбук"
           >
             <a target="_blank" href="https://www.facebook.com">
@@ -55,7 +55,7 @@ const Footer = () => {
             </a>
           </li>
           <li
-            className="hover:scale-105 transition-all"
+            className="hover:scale-110 transition-all"
             title="Перейти на сторінку Ютуб"
           >
             <a target="_blank" title="Перейти на сторінку Ютуб">
@@ -63,6 +63,12 @@ const Footer = () => {
             </a>
           </li>
         </ul>
+      </div>
+
+      <div className="mt-10 pb-2 text-Accent-dark text-center">
+        <small className="w-1/2 block mx-auto border-t-[1px] border-Accent-dark pt-1">
+          2022 - {new Date().getFullYear()}
+        </small>
       </div>
     </footer>
   );
